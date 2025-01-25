679c9ea2bd0c382e02dfea47ed50d1d15408fafd
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2025-01-24 21:20:03 -0600

    Final changes made to my files.

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 const spans = document.querySelectorAll('span');
+const submitBtn = document.querySelector('button');
+const score = document.getElementById('score');
+const ratingMessage = document.querySelector('.rating-state-end');
+let spanValue = 0;
 
-console.log(spans)
+const showRating = (spanValue)=>{
+    score.innerText = spanValue;
+}
+
 
 spans.forEach((span)=>{
     span.addEventListener('click', ()=>{
-        console.log(span.innerText);
+        spanValue = parseInt(span.innerText);
+        showRating(spanValue)
     })
 })
+
+
+
+
+submitBtn.addEventListener('click', ()=>{
+    
+    if (spanValue <= 0){
+        alert('Enter a number')
+        return
+    }
+
+   
+
+    ratingMessage.style.visibility = 'visible';
+    ratingMessage.style.opacity = '1';
+    
+    setTimeout(()=>{
+        ratingMessage.style.visibility = 'hidden';
+        ratingMessage.style.opacity = '0';
+    }, 3000)
+
+    spanValue = 0;
+
+    
+})
+
+/* Create a conditional to validate that the score has beeb selected*/ 
+
+/* Reset the score value*/
